f8496eb1e416f6a3a16270abdc0985f34439a422
nperaino <63611002+nperaino@users.noreply.github.com>
2020-06-05 13:30:28 -0400

    Add files via upload

--- mobility/MainPySigma.py
+++ mobility/MainPySigma.py
@@ -46,32 +46,98 @@ mode). In the latter case the radii are temperature dependent.
 
 from PySigmaFunctions import *
 import tkinter as tk
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, asksaveasfilename 
 from projectionApproximation import *
-
-
-#Ask to open the input geometry commented out to save development time
-#root = tk.Tk()
-#root.withdraw()
-#geometryInputFile = askopenfilename()
-
-geometry = getInputGeometry("c60.inp")
-
-#mode sets the radius correction method as a string: 
-#   There is no real useful reason to use any mode other than 'temperature and size'.
-#   'solid sphere' constant radius, based on vander waals radius of the atom.  This is simple case, and is made available in case future calculations can be absed off of the uncorrected radius.
-#   'temperature'  temperature dependent atom radius.
-#   'temperature and size'  includes temperature, accounts for superposition effects due to increasing number of atoms.  This is the default mode.
-#   Temp cannot be 0, MaxCycles seems to do best over 30 but certainly depends on input.  If you dont give it enough cycles it doesn't converge.
-
-
-#PA = projectionApproximation(geometry, temp=100, accuracy=1, buffr=1.4, maxCycles=100, mode='temperature and size')
-#print(PA)
-
-
-Temp= []
-CCSsolid =[]
 from tqdm import tqdm 
-for i in tqdm(range(50,605,50)):
-    CCSsolid.append(projectionApproximation(geometry, temp=i, accuracy=1, buffr=0.109, maxCycles=50, mode='temperature'))
-    Temp.append(i)
+from PyQt5 import QtWidgets
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
+import sys
+import os
+
+print('This script will calculate the temperature and size dependent collisional cross setion (CCS)')
+print('from an input file generated using Gabedit as a Gaussian input file.  This way, Gabedit or Gaussian')
+print('can be used to optimize the gas phase geometry before calculating the CCS.  It parases by line counting')  
+print('')
+print('')
+print('')
+input('Please press Enter to select an input file')
+root = tk.Tk()
+root.withdraw()
+geometryInputFile = askopenfilename()
+geometry = getInputGeometry("c60.inp")
+print('Enter 1 for single temperature run')
+print('Enter 2 for curve')
+tempType = input('Enter Selection:')
+print(tempType)
+print('')
+print('')
+print('')
+print('Please Enter buffer gas radius.  Use 0.109 if you are not sure.')
+bufferRadius = float(input('Enter buffer gas radius:'))
+print('')
+print('')
+print('')
+print('Please enter an accuracy.  1 is ok for smaller molecules less than 100 atoms')
+print('but larger molecules will take longer so raise up to reduce time')
+acc = float(input('Accuracy: '))
+print('')
+print('')
+print('')
+print('Please enter the maximum number of rotations.  Very symmetric molecules need fewer')
+print('If you are unsure, run a type 2 temperature curve and see if it is very inconsitent.')
+print('50 seems to be ok for small molecules.')
+maxC = int(input('Enter maximum cycles:'))
+
+if tempType == '1':
+    print('Please enter the temperature in Kelvin.')
+    tempK = float(input('Temperature: '))
+    PA = projectionApproximation(geometry, temp = tempK, accuracy=acc, buffr=bufferRadius , maxCycles=maxC, mode='temperature and size')
+    print('The projection area is: ',PA)
+    input('press enter to quit')
+elif tempType == '2':
+    Temp= []
+    CCS =[]
+    for i in tqdm(range(20,605,20)):
+        CCS.append(projectionApproximation(geometry, temp=i, accuracy=acc, buffr=bufferRadius , maxCycles=maxC, mode='temperature and size'))
+        Temp.append(i)
+    print('Please select a save file location:')
+
+    saveData = asksaveasfilename()      
+    CCSdata =[]
+    for i,j in zip(Temp, CCS):
+        CCSdata.append([i,j])
+    with open(saveData+'.csv', 'w', newline='') as csvfile: 
+        csvwriter = csv.writer(csvfile)
+        headers=['Temperature','CCS']
+        # writing the headers 
+        csvwriter.writerow(headers) 
+        # writing the data rows
+        csvwriter.writerows(CCSdata)
+    print('A graph will display the results.')
+    title = input('please input a title for the graph:')
+    class MainWindow(QtWidgets.QMainWindow):
+
+        def __init__(self, *args, **kwargs):
+            super(MainWindow, self).__init__(*args, **kwargs)
+            self.graphWidget = pg.PlotWidget()
+            self.setCentralWidget(self.graphWidget)
+            self.graphWidget.setTitle("<span style=\"color:black;font-size:10px\">"+title+"  </span>")
+            self.graphWidget.setLabel('left', 'CCS (A<sup>2</sup>)', color='black', size=30)
+            self.graphWidget.setLabel('bottom', 'Temperature (K)', color='black', size=30)
+            self.graphWidget.setBackground('w')
+            pen = pg.mkPen(color=(0, 0, 0))
+            self.graphWidget.showGrid(x=True, y=True)
+            self.graphWidget.plot(Temp, CCS, pen=pen)
+
+
+    def main():
+        app = QtWidgets.QApplication(sys.argv)
+        main = MainWindow()
+        main.show()
+        sys.exit(app.exec_())
+
+
+    if __name__ == '__main__':
+        main()
+
